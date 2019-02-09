d41dd6d4543fd0821e024d8b23c469ac3bf7db34
peremozec <kulesh.sergy@gmail.com>
2019-02-09 11:59:37 +0200

    change test for jenkins

--- src/test/java/ua/startit/SignInTest.java
+++ src/test/java/ua/startit/SignInTest.java
@@ -8,45 +8,45 @@ import ua.startit.pageobjects.HomePage;
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SignInTest {
+public class SignInTest extends BaseTest {
 
-    private static final String BASE_URL = "https://www.amazon.com";
-
-    // 1. open HomePage
-    // 2. Click on Sign in button
-    // 3. Fill up login and password input fields
-    // 4. Click on Sign in button
-    // 5. Observe page (Assert)
-
-    private String browser;
-
-    @Factory
-    public Object[] browsers() {
-        return new Object[] {
-                new SignInTest(BrowserType.CHROME),
-                new SignInTest(BrowserType.FIREFOX),
-                new SignInTest(BrowserType.SAFARI)
-        };
-    }
-
-
-    public SignInTest(String browser) {
-        this.browser = browser;
-    }
-
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        Configuration.startMaximized = true;
-        Configuration.browser = this.browser;
-        Configuration.baseUrl = BASE_URL;
-        Configuration.timeout = 10000;
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        close();
-    }
+//    private static final String BASE_URL = "https://www.amazon.com";
+//
+//    // 1. open HomePage
+//    // 2. Click on Sign in button
+//    // 3. Fill up login and password input fields
+//    // 4. Click on Sign in button
+//    // 5. Observe page (Assert)
+//
+//    private String browser;
+//
+//    @Factory
+//    public Object[] browsers() {
+//        return new Object[] {
+//                new SignInTest(BrowserType.CHROME),
+//                new SignInTest(BrowserType.FIREFOX),
+//                new SignInTest(BrowserType.SAFARI)
+//        };
+//    }
+//
+//
+//    public SignInTest(String browser) {
+//        this.browser = browser;
+//    }
+//
+//
+//    @BeforeMethod(alwaysRun = true)
+//    public void setUp() {
+//        Configuration.startMaximized = true;
+//        Configuration.browser = this.browser;
+//        Configuration.baseUrl = BASE_URL;
+//        Configuration.timeout = 10000;
+//    }
+//
+//    @AfterMethod(alwaysRun = true)
+//    public void tearDown() {
+//        close();
+//    }
 
     @Test
     public void signTest() {
